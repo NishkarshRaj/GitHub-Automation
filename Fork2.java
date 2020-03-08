87316cfdc4668b0263bb41f9d59e81c0ba34bc96
Nishkarsh Raj <500060720@stu.upes.ac.in>
2020-03-08 19:16:53 +0530

    Create Fork2.java

---
+++ Fork2.java
@@ -0,0 +1,28 @@
+package midsem;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class Fork2
+{
+	public static void main(String [] args) throws InterruptedException
+	{
+		System.setProperty("webdriver.chrome.driver", "D:\\UPES DevOps\\DevOps Sem 6\\Test Automation\\Softwares\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.navigate().to("https://github.com/login");
+		driver.manage().window().maximize();
+		WebElement element; 
+		element = driver.findElement(By.name("login"));
+		element.sendKeys("MeghaRawat3");
+		element = driver.findElement(By.name("password"));
+		element.sendKeys("Megh4@123");
+		driver.findElement(By.name("commit")).click();
+		
+		//Navigate to repository to be forked
+		driver.navigate().to("https://github.com/Nishkarsh007/Test-Automation");
+		//Fork the repository
+		driver.findElement(By.xpath("/html/body/div[4]/div/main/div[2]/div/ul/li[3]/form/button")).click();
+		//Create new branch
+		Thread.sleep(10000); // Wait till forking is completed - 10 seconds
+	}
+}
