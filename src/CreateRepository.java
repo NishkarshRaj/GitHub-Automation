d626c58d3cf1a11404562496b346a0680f14c88d
Nishkarsh Raj <500060720@stu.upes.ac.in>
2020-03-13 08:06:54 +0530

    Update CreateRepository.java

--- src/CreateRepository.java
+++ src/CreateRepository.java
@@ -12,26 +12,31 @@ public class CreateRepository
 		driver.navigate().to("https://github.com/login");
 		driver.manage().window().maximize();
 		WebElement element; 
+		
+		
+		//Login
 		element = driver.findElement(By.name("login"));
 		element.sendKeys("nishkarshraj000@gmail.com");
 		element = driver.findElement(By.name("password"));
 		element.sendKeys("Nish123!@#");
 		driver.findElement(By.name("commit")).click();
 		
+		
 		//Create Repository
 		driver.findElement(By.xpath("/html/body/div[4]/div/aside[1]/div[2]/div[1]/div/h2/a")).click();		
 		element = driver.findElement(By.xpath("//*[@id=\"repository_name\"]"));		
 		element.sendKeys("Test Automation");
 		driver.findElement(By.xpath("//*[@id=\"new_repository\"]/div[3]/button")).submit();	
+	
 		
 		//Add Default file to make the repository forkable because empty repository cannot be forked
-		driver.findElement(By.xpath("//*[@id=\"js-repo-pjax-container\"]/div[3]/div/div[1]/div[1]/p/a[1]")).click();
+		driver.findElement(By.xpath("//*[@id=\"js-repo-pjax-container\"]/div[2]/div/div[1]/div[1]/p/a[1]")).click();
 		//File name
-		element = driver.findElement(By.xpath("//*[@id=\"js-repo-pjax-container\"]/div[3]/div/div/form[2]/div[1]/span/input"));		
+		element = driver.findElement(By.name("filename"));		
 		element.sendKeys("README.md");
 		//Content
-		element = driver.findElement(By.xpath("//*[@id=\"js-repo-pjax-container\"]/div[3]/div/div/form[2]/div[5]/div[2]/div/div[5]/div[1]/div/div/div/div[5]/div/pre"));		
+		element = driver.findElement(By.xpath("//*[@id=\"js-repo-pjax-container\"]/div[2]/div/div/form[2]/div[5]/div[2]/div/div[5]/div[1]/div/div/div/div[5]/div/pre"));		
 		element.sendKeys("Default Content");
-		driver.findElement(By.xpath("//*[@id=\"submit-file\"]")).click();				
+		driver.findElement(By.id("submit-file")).click();				
 	}
 }
